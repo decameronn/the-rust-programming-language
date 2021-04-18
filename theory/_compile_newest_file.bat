ecae41e173392f72e83ea66b2f99cc95b32b1261
Constantin Zorile <czorile@gmail.com>
2021-04-18 22:06:57 +0300

    remove surplus comments

--- theory/_compile_newest_file.bat
+++ theory/_compile_newest_file.bat
@@ -1,12 +1,11 @@
 :: @brief Find the newest .rs file in a folder and compile it with rustc
 :: @author Decameron
+
 @echo off
 for /f "delims=" %%f in ('dir /b /a-d /od /tw %cd%\*.rs') do (
   set file=%%f
 )
-echo The newest file is: %file%
-echo Compiling %file% with rustc...
-call %userprofile%\.cargo\bin\rustc %file%
 
-:: optionally, we can set the destination path for the .exe file:
-:: echo %file% > x:\destination\path\to\file.exe
+echo The newest file is: %file%
+echo Compiling %file% with RUSTC...
+call %userprofile%\.cargo\bin\rustc %file%
