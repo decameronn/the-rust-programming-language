61aeec8ba007d8af3214cbfae978772fae1a64c0
Constantin Zorile <czorile@gmail.com>
2021-04-30 14:41:12 +0300

    reorganize project with Cargo

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-424107ab7324a846.d
@@ -0,0 +1,5 @@
+d:\code\the-rust-programming-language\theory\hello_cargo\target\rls\debug\deps\hello_cargo-424107ab7324a846.rmeta: src\main.rs
+
+d:\code\the-rust-programming-language\theory\hello_cargo\target\rls\debug\deps\hello_cargo-424107ab7324a846.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-c5c71599f5c68193.d
@@ -0,0 +1,5 @@
+d:\code\the-rust-programming-language\theory\hello_cargo\target\rls\debug\deps\hello_cargo-c5c71599f5c68193.rmeta: src\main.rs
+
+d:\code\the-rust-programming-language\theory\hello_cargo\target\rls\debug\deps\hello_cargo-c5c71599f5c68193.d: src\main.rs
+
+src\main.rs:
